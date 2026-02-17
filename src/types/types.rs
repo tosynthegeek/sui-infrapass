e6c09076a02f6cf04baba0b10c48d69e7e01afc9
tosynthegeek <oluwatosinojedapo@gmail.com>
2026-02-17 16:54:58 +0100

    impl for deriving tier config from u8

--- src/types/types.rs
+++ src/types/types.rs
@@ -1,3 +1,5 @@
+use anyhow::{Ok, Result, anyhow};
+
 use crate::types::coin::CoinType;
 
 pub enum TierConfigInput {
@@ -11,4 +13,36 @@ pub struct TierInfo {
     pub coin_type: CoinType,
     pub price: u64,
     pub tier_type_string: String,
+}
+
+impl TierConfigInput {
+    pub fn from_u8(
+        tier: &u8,
+        duration: &Option<u64>,
+        quota: &Option<u64>,
+        unit_price: &Option<u64>,
+    ) -> Result<Self> {
+        match tier {
+            0 => {
+                let duration_ms = duration.ok_or_else(|| anyhow!("invalid duration provided"))?;
+
+                Ok(TierConfigInput::Subscription { duration_ms })
+            }
+            1 => {
+                let quota_limit = quota.ok_or_else(|| anyhow!("invalid quota limit provided"))?;
+                let duration_ms = duration.ok_or_else(|| anyhow!("invalid duration provided"))?;
+
+                Ok(TierConfigInput::Quota {
+                    quota_limit,
+                    duration_ms,
+                })
+            }
+            2 => {
+                let price_per_unit =
+                    unit_price.ok_or_else(|| anyhow!("invalid unit price provided"))?;
+                Ok(TierConfigInput::UsageBased { price_per_unit })
+            }
+            _ => Err(anyhow!("Invalid tier selected")),
+        }
+    }
 }
