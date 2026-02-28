6c093d96d78c58cf0c261af2c33a281403f058ba
tosynthegeek <oluwatosinojedapo@gmail.com>
2026-02-28 13:45:40 +0100

    ci: add Docker workflow to build and publish sidecar image

--- src/utils/address.rs
+++ src/utils/address.rs
@@ -4,7 +4,6 @@ use sui_types::base_types::SuiAddress;
 use crate::utils::config::{default_wallet_config, load_wallet_context};
 
 pub fn get_sender_address() -> Result<SuiAddress> {
-    // TODO: Check from cache or memory where we store
     let default_path = default_wallet_config()?;
 
     let mut wallet = load_wallet_context(default_path)?;
